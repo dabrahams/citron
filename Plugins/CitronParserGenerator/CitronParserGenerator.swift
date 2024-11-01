98b775ca505f8a8c446090b1ba5ba4b330548be8
Dave Abrahams <dabrahams@adobe.com>
2024-11-01 11:59:04 -0700

    Reproducer

--- Plugins/CitronParserGenerator/CitronParserGenerator.swift
+++ Plugins/CitronParserGenerator/CitronParserGenerator.swift
@@ -5,13 +5,13 @@ struct CitronParserGenerator: BuildToolPlugin {
 
   func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
     guard let target = target as? SourceModuleTarget else { return [] }
-    let inputFiles = target.sourceFiles.filter({ [ "citron", "y"].contains($0.path.extension) })
+    let inputFiles = target.sourceFiles.filter({ [ "citron", "y"].contains($0.url.extension) })
     return try inputFiles.map {
       let inputFile = $0
       let inputPath = inputFile.path
       let outputName = inputPath.stem + ".swift"
       let outputPath = context.pluginWorkDirectory.appending(outputName)
-      return .buildCommand(
+      return Command.buildCommand(
         displayName: "Generating \(outputName) from \(inputPath.lastComponent)",
         executable: try context.tool(named: "citron").path,
         arguments: [ "\(inputPath)", "-o", "\(outputPath)" ],
